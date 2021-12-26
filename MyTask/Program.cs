956829cc5693800db17f43c9a98de284439e0574
Egor <fomin0596@gmail.com>
2021-12-26 21:14:43 +0300

    program(ver 1.0)

--- MyTask/Program.cs
+++ MyTask/Program.cs
@@ -1,7 +1,26 @@
-﻿int[] array = { 2, 0, -18, 24, 7, 8, 10, 4, -8, 25 };
-int[] newArray = {};
+﻿void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(-50,50);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+       Console.Write($"{numbers[i]} ");
+    }
+}
+
+int[] array = new int[20];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
 int size = array.Length;
 for (int i = 0; i < size; i++)
 {
-    if (array[i] > 8)
+    if (array[i] > 8) Console.Write($"{array[i]} ");
+    
 }
+Console.WriteLine();
